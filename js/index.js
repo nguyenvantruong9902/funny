cb7070e2f91ab3965a46e0123245c8b61825011f
nguyenvantruong9902 <nguyenvantruong9902@gmail.com>
2021-08-25 12:58:27 +0700

    add sexy girl

--- js/index.js
+++ js/index.js
@@ -9,6 +9,21 @@ function onHoverOnName() {
      name.style.color = `#${color}`;
 }
 
+function onClickName() {
+     document.getElementById("content").innerHTML = `
+          <div class='left-content' data-toggle="modal" data-target=".bd-example-modal-sm" onclick="onClickVeryFunny()">
+               <p>L</p>
+               <p>Ầ</p>
+               <p>Y</p>
+          </div>
+          <div class='right-content' data-toggle="modal" data-target=".bd-example-modal-sm" onclick="onClickFunny()">
+               <p>B</p>
+               <p>Ự</p>
+               <p>A</p>
+          </div>
+     `;
+}
+
 function onInputLover() {
      const iLU = "I Love You!";
      const lover = document.getElementById("lover");
@@ -26,8 +41,7 @@ function onInputLover() {
 function onSubmitLover() {
      const lover = document.getElementById("lover");
      const results = document.getElementById("results");
-     const image = document.createElement("img");
-     image.className = "lover-result";
+
      if (!lover.value) {
           results.innerHTML = `
           <img class="lover-result" src="assets/angry-icon.png" />
@@ -37,4 +51,41 @@ function onSubmitLover() {
           <img class="lover-result" src="assets/funny-icon.png" />
           `;
      }
+}
+
+function onClickFunny() {
+     results.innerHTML = `
+     <img src="assets/sexy-girl.png" />
+     <img class='left-breast' src="assets/left-breast.png" onclick="onClickLeftBreast()"/>
+     <img class='right-breast' src="assets/right-breast.png" onclick="onClickRightBreast()"/>
+     `;
+}
+
+function playAudio() {
+     new Audio("assets/squeeze.wav").play();
+}
+
+function onClickLeftBreast() {
+     const leftBreast = document.querySelector(".left-breast");
+     leftBreast.style.top = "170px";
+     playAudio();
+     setTimeout(() => {
+          leftBreast.style.top = "180px";
+     }, 150);
+}
+
+function onClickRightBreast() {
+     const rightBreast = document.querySelector(".right-breast");
+     rightBreast.style.top = "162px";
+     playAudio();
+     setTimeout(() => {
+          rightBreast.style.top = "172px";
+     }, 150);
+}
+
+function onClickVeryFunny() {
+     const results = document.getElementById("results");
+     results.innerHTML = `
+     Very funny
+     `;
 }
